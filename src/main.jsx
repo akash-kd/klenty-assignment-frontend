dd7cf42d49daaf2c4012b887cfbea4df6f5f4f2d
akash-kd <39560107+akash-kd@users.noreply.github.com>
2024-02-21 00:18:48 +0530

    Main Changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import { UserProvider } from './context/userContext.jsx'
+import { ImageProvider } from './context/imagesContext.jsx'
+
 
 import {
 	QueryClient,
@@ -12,8 +16,13 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-		</QueryClientProvider>
+		<UserProvider>
+			<ImageProvider>
+				<QueryClientProvider client={queryClient}>
+					<App />
+					<Toaster />
+				</QueryClientProvider>
+			</ImageProvider>
+		</UserProvider>
 	</React.StrictMode>,
 )
