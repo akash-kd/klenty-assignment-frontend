2410d4ce9b49a8f7cf8bf24e8ce949f015863c61
akash-kd <39560107+akash-kd@users.noreply.github.com>
2024-02-21 00:19:27 +0530

    Adding Routes, Main & Favorites Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,231 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Icons from './components/icons/Icons'
+import Login from './pages/auth/login'
+import Signup from './pages/auth/signup'
+import { TypeAnimation } from 'react-type-animation'
+import { useRef, useState } from 'react'
+import { useUser } from './context/userContext'
+import Search from './api/unsplash/search'
+import { useImages } from './context/imagesContext'
+import Main from './pages/main'
+import { useNavigate } from 'react-router-dom'
+import Favorites from './pages/favourites'
+
+function TopBar() {
+	const aref = useRef()
+	const { user, logout } = useUser()
+	const { query, setQuery, addImages, getAllImages } = useImages()
+
+	const onSearch = async () => {
+		const res = await Search({ query: query, per_page: 20 })
+		addImages(query, res.data.results)
+	}
+
+
+	const searchBar = (
+		<>
+			<img src='/unspashlogo.png' className='h-6' />
+
+			<div className='flex w-3/5 min-w-3/5 h-full justify-center items-center'>
+				<Icons.CharmSearch />
+				<input
+					value={query}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') {
+							onSearch();
+						}
+					}}
+					onChange={(e) => setQuery(e.target.value)}
+					placeholder='Search the images'
+					className='text-black px-1 py-2 w-full h-full outline-none border-t-2 border-b-2 font-semibold'
+				/>
+				<Icons.Right />
+			</div>
+			<a
+				ref={aref}
+				onClick={onSearch}
+				className='flex justify-center items-center h-full gap-3 px-5 border-2 border-gray-900 bg-black rounded-full text-white font-semibold'
+			>
+				<Icons.SearchIcon /> Search
+			</a>
+		</>
+	)
+	
+	if (user) {
+		return (
+			<div className='flex justify-between items-center w-full h-16 absolute top-0 px-10 font-semibold text-gray-400'>
+				<div className='flex justify-center items-center h-10 gap-5'>
+					{getAllImages()?.length > 0 ? searchBar : <></>}
+				</div>
+				<div className='flex gap-10 justify-center items-center'>
+					<a
+						href='/'
+						className='hover:text-black hover:underline underline-offset-8 underline-2'
+					>
+						{user.email}
+					</a>
+					<a
+						href='/'
+						className='hover:text-black hover:underline underline-offset-8 underline-2'
+					>
+						Search
+					</a>
+					<a
+						href='/favorites'
+						className='hover:text-black hover:underline underline-offset-8 underline-2'
+					>
+						Favorites
+					</a>
+					<a
+						onClick={logout}
+						href='/'
+						className='flex gap-2 items-center justify-center bg-black text-white px-4 py-1 rounded-full drop-shadow-md'
+					>
+						Logout
+						<Icons.CharmChevronRight />
+					</a>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div className='flex justify-between items-center w-full h-16 absolute top-0 px-10 gap-10 font-semibold text-gray-400'>
+			<div className='flex justify-center items-center h-10 gap-10'>
+				{getAllImages()?.length > 0 ? searchBar : <></>}
+			</div>
+			<div className='flex gap-10 justify-center items-center'>
+				<a
+					href='/'
+					className='hover:text-black hover:underline underline-offset-8 underline-2'
+				>
+					Search
+				</a>
+				<a
+					href='/login'
+					className='hover:text-black hover:underline underline-offset-8 underline-2'
+				>
+					Login
+				</a>
+				<a
+					href='/signup'
+					className='flex gap-2 items-center justify-center bg-black text-white px-4 py-1 rounded-full drop-shadow-md'
+				>
+					Sign Up
+					<Icons.CharmChevronRight />
+				</a>
+			</div>
+		</div>
+	)
+}
+
+function Home() {
+	const [current, setCurrent] = useState()
+	const [search, setSearch] = useState()
+	const { addImages } = useImages()
+	const nav = useNavigate()
+
+	const onSearch = async () => {
+		const res = await Search({ query: search, per_page: 30 })
+		nav(`/search`)
+		addImages(search, res.data.results)
+	}
+
+	const onRandom = async () => {
+		const res = await Search({ query: current, per_page: 30 })
+		nav(`/search`)
+		addImages(current, res.data.results)
+	}
+
+	return (
+		<div className='flex flex-col w-full h-full justify-center items-center'>
+			<img src='/unspashlogo.png' alt='Unsplash Logo' width='320px' />
+			<div className='flex w-2/5 justify-center items-center my-14'>
+				<Icons.CharmSearch />
+				<input
+					value={search}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') {
+							onSearch()
+						}
+					}}
+					onChange={(e) => setSearch(e.target.value)}
+					placeholder='Search the images'
+					className='px-1 py-2 w-full outline-none border-t-2 border-b-2 font-semibold'
+				/>
+				<Icons.Right />
+			</div>
+			<div className='flex gap-4'>
+				<button
+					onClick={onSearch}
+					className='flex justify-center items-center gap-3 px-5 py-2 border-2 border-gray-900 bg-black rounded-full text-white drop-shadow-lg font-semibold'
+				>
+					<Icons.SearchIcon /> Search
+				</button>
+				<button
+					onClick={onRandom}
+					className='flex justify-center items-center gap-3 px-5 py-2 border-2 border-gray-900 bg-black rounded-full text-white drop-shadow-lg font-semibold'
+				>
+					<Icons.CharmGitCompare />
+					<TypeAnimation
+						preRenderFirstString={true}
+						sequence={[
+							() => setCurrent('Cars'),
+							'Random - Cars',
+							3000,
+							() => setCurrent('Adventure'),
+							'Random - Adventure',
+							3000,
+							() => setCurrent('Mountains'),
+							'Random - Mountains',
+							3000,
+							() => setCurrent('Space'),
+							'Random - Space',
+							3000,
+							() => setCurrent('Earth'),
+							'Random - Earth',
+							3000,
+						]}
+						wrapper='span'
+						speed={6}
+						repeat={Infinity}
+					/>
+				</button>
+			</div>
+		</div>
+	)
+}
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/signup',
+		element: <Signup />,
+	},
+	{
+		path: '/search',
+		element: <Main />,
+	},
+	{
+		path: '/favorites',
+		element: <Favorites />,
+	},
+])
+
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+	return (
+		<>
+			<TopBar />
+			<RouterProvider router={router} />
+		</>
+	)
 }
 
 export default App
---
+++ src/pages/favourites/index.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react'
+import { DelFav, GetFavs } from '../../api/favourites/favs'
+import { useUser } from '../../context/userContext'
+import toast from 'react-hot-toast'
+import Icons from '../../components/icons/Icons'
+
+function Card({ image, setImage }) {
+	const { user } = useUser()
+
+	const onRemove = async () => {
+		const res = DelFav(image.imageid)
+		res.then(() => {
+			toast.success('Removed from favorites')
+			const result = GetFavs(user?._id)
+			result
+				.then((res) => {
+					setImage(res.data.data)
+				})
+				.catch((err) => {
+					toast.error(err.response.data.message)
+				})
+		}).catch((err) => {
+			toast.error(err.response.data.message)
+		})
+	}
+	return (
+		<div className='flex flex-col rounded-lg border border-gray-400'>
+			<img
+				className='w-full rounded-t-lg'
+				src={image.urls.small}
+				alt={image.alt_description}
+			/>
+			<div
+				onClick={onRemove}
+				className='flex justify-center items-center gap-2 h-10 bg-gray-200 text-gray-900 rounded-b-lg font-semibold tracking-tight cursor-pointer hover:underline underline-offset-4 underline-2'
+			>
+				<Icons.CharmStar />
+				<h2>Remove from Favorites</h2>
+			</div>
+		</div>
+	)
+}
+
+export default function Favorites() {
+	const { user } = useUser()
+	const [images, setImages] = useState([])
+	const col1 = []
+	const col2 = []
+	const col3 = []
+	const col4 = []
+
+	useEffect(() => {
+		const res = GetFavs(user?._id)
+		res.then((res) => {
+			setImages(res.data.data)
+		}).catch((err) => {
+			toast.error(err.response.data.message)
+		})
+	}, [])
+
+	if (images.length > 0) {
+		for (let i = 0; i < images.length; i++) {
+			const image = images[i]
+			if (i % 4 == 0)
+				col1.push(<Card image={image} setImage={setImages} />)
+			else if (i % 4 == 1)
+				col2.push(<Card image={image} setImage={setImages} />)
+			else if (i % 4 == 2)
+				col3.push(<Card image={image} setImage={setImages} />)
+			else if (i % 4 == 3)
+				col4.push(<Card image={image} setImage={setImages} />)
+		}
+
+		return (
+			<div className='flex w-full h-full pt-20 px-10'>
+				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+					<div className='flex flex-col gap-4'>{col1}</div>
+					<div className='flex flex-col gap-4'>{col2}</div>
+					<div className='flex flex-col gap-4'>{col3}</div>
+					<div className='flex flex-col gap-4'>{col4}</div>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div className='w-full h-full flex justify-center items-center'>
+			No Images
+		</div>
+	)
+}
